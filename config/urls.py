110563259cbf265baf691ec360b3751e08bc3d3a
OpalescentHearts <109106306+OpalescentHearts@users.noreply.github.com>
2022-12-08 15:56:29 -0600

    Boop

--- config/urls.py
+++ config/urls.py
@@ -5,8 +5,8 @@ from app.views import *
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", views.home),
-    path("aboutme/", views.about),
-    path("projects", views.projects),
-    path("skills", views.skills),
+    path("", views.home, name="home"),
+    path("aboutme/", views.about, name="about"),
+    path("projects", views.projects, name="projects"),
+    path("skills", views.skills, name="skills"),
 ]
